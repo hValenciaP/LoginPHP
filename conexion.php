37799badac494e178816598fe1c7d6404a89ddc0
Heyner <valencia.preciadoh@gmail.com>
2022-07-09 19:30:26 -0500

    Curso Login PHP

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+include("config.php");
+$conexion = new mysqli($server, $user, $pass, $bd);
+if(mysqli_connect_errno()){
+    echo "No conectado".mysqli_connect_error();
+    exit();
+}else{
+    //echo "Conectado";
+}
+?>
